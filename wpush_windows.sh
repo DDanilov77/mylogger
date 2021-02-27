5036af2680d63d38dda5211dd31a1d494b0936c9
Denis Danilov <ddanilov77@mail.ru>
2021-02-27 22:25:11 +0300

    Commit by HOME20/denis at

--- wpush_windows.sh
+++ wpush_windows.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
-git pull origin master
+git pull
 
 file=".git/new-push.txt"
 git status > $file
 git add .
 
 if [ -z "$1" ]; then  #if not yet cmd parameters
-    var1=0; var2=0; var3=0;
     git commit -m"Commit by ${HOSTNAME}/${USERNAME} at ${DATE}"
 else
-    #echo "$1";
     git commit -m"$1 : by ${HOSTNAME}/${USERNAME} at ${DATE}";
 fi
-git push origin master
+git push
